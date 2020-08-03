1928a73d88e02b5db8285b99145649218c868f34
UrielBatista <49211381+UrielBatista@users.noreply.github.com>
2020-08-03 12:12:50 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from selenium.common.exceptions import NoSuchElementException
+from time import sleep
+from scraping import Scraping
+
+if __name__ == '__main__':
+    try:
+        scraping = Scraping()
+        scraping.scrap_and_insert()
+        sleep(1)
+        
+        
+
+        print('The And!!')
+
+    except Exception as exception:
+        print(exception)
+
+    finally:
+        pass
+
+
+
